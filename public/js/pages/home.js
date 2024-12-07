c2a67756e2885450ce7318fadc86306ba4056ba7
stocksManagement <slimani.abdelhadi08@gamil.com>
2024-12-07 11:53:16 +0100

    ttt

---
+++ public/js/pages/home.js
@@ -0,0 +1,30 @@
+
+  function home() {
+    let elmnt = 'rrrr';
+    return (<div id="contentPg">
+        <h1 class="p-relative">maison page</h1>
+        <ul class="linksDv">
+          <li>
+            <i class="stock"></i>
+            Tableau de bord [F1]
+          </li>
+          <li>
+            <i class="fa-solid fa-user-group"></i>
+            Clients et Fournisseurs [F2]
+          </li>
+          <li>
+            <i class="fa-solid fa-headset"></i>
+            utilisateurs [F3]
+          </li>
+          <li>
+            <i class="stock"></i>
+            stock [F4]
+          </li>
+
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>);
+  }
